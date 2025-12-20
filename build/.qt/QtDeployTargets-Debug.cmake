f4cc2b846e673c0d9c6b636a7c73fa31752cbc00
MineSantum <minesantum@gmail.com>
2025-12-20 14:38:46 +0100

    funciona

---
+++ build/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_custom-export-panel_FILE C:/Users/DonKolia/Downloads/Custom Export Panel/build/Debug/custom-export-panel.dll)
+set(__QT_DEPLOY_TARGET_custom-export-panel_TYPE MODULE_LIBRARY)
+set(__QT_DEPLOY_TARGET_custom-export-panel_RUNTIME_DLLS C:/Qt/6.8.3/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.3/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.3/msvc2022_64/bin/Qt6Cored.dll)
